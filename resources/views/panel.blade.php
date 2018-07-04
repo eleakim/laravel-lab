c6813b4aafb9c427f5ef8d9d02f12fd41a80b332
Pedro Henrique <pedro.franca@mktvirtual.com.br>
2018-07-04 17:27:46 -0300

    bsbadmin

---
+++ resources/views/panel.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('content')
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+                <h2>BLANK PAGE</h2>
+            </div>
+        </div>
+    </section>
+@endsection
